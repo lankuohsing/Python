b8c8d4034f6ba1936a606f88ce8d86c20dcfd79b
lankuohsing <lankuohsing@163.com>
2017-01-26 22:32:21 +0800

    创建第一个python例子

---
+++ test3-0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 22:20:32 2017
+
+@author: lankuohsing
+"""
+
+# this is the first comment
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+print(text)
