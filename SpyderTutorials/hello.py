597bdd6b4a2bc5549f8cd3de3efc2a116d2efee0
lankuohsing <lankuohsing@163.com>
2017-01-31 18:05:39 +0800

    添加ez_setup.py

---
+++ SpyderTutorials/hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 22:50:39 2017
+
+@author: lankuohsing
+"""
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None"""
+    print("Laters World")
+    #print("Hello World2")
+
+# main program starts here
+hello()
