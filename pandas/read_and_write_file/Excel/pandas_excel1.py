ab77684638c9ce5a2647abbd56bc20c1d0bd40c6
languoxing <languoxing@126.com>
2017-12-31 22:45:17 +0800

    添加计算簇内误差平方和

---
+++ pandas/read_and_write_file/Excel/pandas_excel1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 28 21:22:39 2017
+
+@author: lankuohsing
+"""
+
+# In[]
+import pandas as pd
+
+# In[]
+xlsx_file1=pd.read_excel("excel_sample1.xlsx",sheetname=1,header=None)
+# In[]
