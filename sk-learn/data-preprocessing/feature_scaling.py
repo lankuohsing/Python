07d3caa0ac3dd95a404c75068cdeba0783588d8a
languoxing <languoxing@126.com>
2018-01-18 15:07:20 +0800

    add feature_range

--- sk-learn/data-preprocessing/feature_scaling.py
+++ sk-learn/data-preprocessing/feature_scaling.py
@@ -8,19 +8,20 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 # In[]
 data = np.array([[-1, 2], [-0.5, 6], [0, 10], [1, 18]])#数据样例
-feature_range=(0,1)#归一化的范围
+feature_range=(0,1)#归一化的范围,类型为tuple
 # In[]
 """
 X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
 X_scaled = X_std * (max - min) + min
 """
-scaler = MinMaxScaler(copy=True)
+scaler = MinMaxScaler(copy=True,feature_range=feature_range)#copy=True保留原始数据矩阵
 scaled_data=scaler.fit_transform(data)
 data_orign=(scaled_data-feature_range[0])/(feature_range[1]-feature_range[0])\
-*(scaler.data_max_-scaler.data_min_)+scaler.data_min_
+*(scaler.data_max_-scaler.data_min_)+scaler.data_min_#恢复成原来的数据
 # In[]
 print(scaler.data_min_)
 print(scaler.data_max_)
 print(scaled_data)
+#print(scaler.data_range_)
 # In[]
 print(feature_range[0])
