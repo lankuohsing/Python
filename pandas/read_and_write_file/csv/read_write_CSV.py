2c6a144844831d883a1d0c34dac8f25f6ae4e4fd
lankuohsing <lankuohsing@163.com>
2018-04-22 22:28:51 +0800

    # fix small bugs

--- pandas/read_and_write_file/csv/read_write_CSV.py
+++ pandas/read_and_write_file/csv/read_write_CSV.py
@@ -16,7 +16,7 @@ iris_label=np.reshape(iris_df.iloc[:,4].as_matrix(),(-1,1))
 # In[]
 path1='D:/Projects/Github/Python/pandas/read_and_write_file/csv/iris1.csv'
 header=['sepal length in cm',	'sepal width in cm','petal length in cm',
-        'al width in cm','class label']
+        'petal width in cm','class label']
 iris1_np=np.hstack((iris_np, iris_label))
 iris1_df=pd.DataFrame(iris1_np)
 iris1_df.to_csv(path1,sep=',',header=header,index_label =range(iris_np.shape[0]))
