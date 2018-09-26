be5b266ac23fba512db20efd55f883753366f499
l00467141 <languoxing@huawei.com>
2018-09-26 20:54:10 +0800

    add read_excel

--- file_operation_examples/read_and_save_files/read_excel.py
+++ file_operation_examples/read_and_save_files/read_excel.py
@@ -16,13 +16,16 @@ print(workbook.sheet_names()) # [u'sheet1', u'sheet2']
 sheet2_name= workbook.sheet_names()[1]
 print(sheet2_name)
 # 根据sheet索引或者名称获取sheet内容
-sheet2 = workbook.sheet_by_name('Sheet2')
+# In[]
+sheet2 = workbook.sheet_by_name("M")
 # sheet的名称，行数，列数
 print(sheet2.name,sheet2.nrows,sheet2.ncols)
+# In[]
 rows = sheet2.row_values(3) # 获取第四行内容
 cols = sheet2.col_values(2) # 获取第三列内容
 print(rows)
 print(cols)
+# In[]
 #获取单元格内容的三种方法
 print(sheet2.cell(1,0).value.encode('utf-8'))
 print(sheet2.cell_value(1,0).encode('utf-8'))
