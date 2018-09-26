3e938d7b74e4c7ed1a15987c4e46a8b5e28212de
l00467141 <languoxing@huawei.com>
2018-09-26 20:29:06 +0800

    add read_excel

---
+++ file_operation_examples/read_and_save_files/read_excel.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 26 20:03:40 2018
+
+@author: l00467141
+"""
+
+# In[]
+import xlrd
+workbook = xlrd.open_workbook(r'English Words Notes.xlsx')
+# In[]
+print(workbook.sheet_names()) # [u'sheet1', u'sheet2']
+
+# In[]
+#获取sheet2
+sheet2_name= workbook.sheet_names()[1]
+print(sheet2_name)
+# 根据sheet索引或者名称获取sheet内容
+sheet2 = workbook.sheet_by_name('Sheet2')
+# sheet的名称，行数，列数
+print(sheet2.name,sheet2.nrows,sheet2.ncols)
+rows = sheet2.row_values(3) # 获取第四行内容
+cols = sheet2.col_values(2) # 获取第三列内容
+print(rows)
+print(cols)
+#获取单元格内容的三种方法
+print(sheet2.cell(1,0).value.encode('utf-8'))
+print(sheet2.cell_value(1,0).encode('utf-8'))
+print(sheet2.row(1)[0].value.encode('utf-8'))
+# 获取单元格内容的数据类型
+print(sheet2.cell(1,3).ctype)
