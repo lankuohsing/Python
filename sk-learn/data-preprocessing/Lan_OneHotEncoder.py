d7b9f7e963c80401556d1a8ba6761227fe0a207e
l00467141 <languoxing@huawei.com>
2018-09-29 16:58:36 +0800

    fix some bugs

--- sk-learn/data-preprocessing/Lan_OneHotEncoder.py
+++ sk-learn/data-preprocessing/Lan_OneHotEncoder.py
@@ -8,13 +8,17 @@ Created on Sat Sep 29 01:11:22 2018
 import numpy as np
 # In[]
 class OneHotEncoder(object):
+    """
+    feature_list_list：存了每列特征的原始向量，列表的每个元素是一个列表，
+                       列表中是对应特征的所有取值（不重复）
+    """
     def __init__(
             self
             ):
         self.row=None
         self.cols=None
         # In[]
-        self.ori_list_list=[]
+        self.ori_np_list=[]
         self.ori_set_list=[]
         self.feature_list_list=[]
         
@@ -24,8 +28,8 @@ class OneHotEncoder(object):
             ):
         self.rows,self.cols=X_ori.shape
         for i in range(self.cols):
-            self.ori_list_list.append(list(X_ori[:,i]))
-            self.ori_set_list.append(set(self.ori_list_list[i]))
+            self.ori_np_list.append(list(X_ori[:,i]))
+            self.ori_set_list.append(set(self.ori_np_list[i]))
             self.feature_list_list.append(list(self.ori_set_list[i]))
             
     def transform(
@@ -34,19 +38,18 @@ class OneHotEncoder(object):
             ):
         # In[]
         one_hot_feature_list=[]
-        one_hot_cols_list=[]
+        Y_rows,Y_cols=Y_ori.shape
         for i in range(self.cols):
-            cols_i=len(self.feature_list_list[i])
-            np_i=np.zeros((self.rows,cols_i))
-            for j in range(self.rows):
+            np_i=np.zeros((Y_rows,len(self.feature_list_list[i])))
+            for j in range(Y_rows):
                 if(Y_ori[j,i] in self.feature_list_list[i]):
                     np_i[j,self.feature_list_list[i].index(Y_ori[j,i])]=1
             one_hot_feature_list.append(np_i)   
         # In[]
-        one_hot_feature=one_hot_feature_list[0]   
+        one_hot_feature_np=one_hot_feature_list[0]   
         for i in range(1,len(one_hot_feature_list)):
-            one_hot_feature=np.hstack((one_hot_feature,one_hot_feature_list[i])) 
-        return one_hot_feature
+            one_hot_feature_np=np.hstack((one_hot_feature_np,one_hot_feature_list[i])) 
+        return one_hot_feature_np
     def fit_transform(
             self,
             X_ori
@@ -59,6 +62,8 @@ if __name__=="__main__":
 
     X_ori =np.array([['Male', 1], ['Female', 3,], ['Female', 2]])
     Y_ori =np.array([['Male', 4], ['a', 3,], ['Female', 2]])
+    Z_ori =np.array([['Male', 4]])
     a=OneHotEncoder()
     c=a.fit_transform(X_ori)
     b=a.transform(Y_ori)
+    d=a.transform(Z_ori)
