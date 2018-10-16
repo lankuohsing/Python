e9c39bd58ddc399d015ae3faebb016e8199e976e
l00467141 <languoxing@huawei.com>
2018-10-16 10:45:06 +0800

    add f.truncate

--- file_operation_examples/read_and_save_files/read_and_save_txt.py
+++ file_operation_examples/read_and_save_files/read_and_save_txt.py
@@ -42,7 +42,13 @@ for line in open("txt_data.txt","r"): #设置文件对象并读取每一行文�
 f4 = open("txt_data.txt","r")   #设置文件对象
 data4 = f4.readlines()  #直接将文件中按行读到list里，效果与方法2一样
 f4.close()             #关闭文件
+"""
+三、清空文件内容
 
+f.truncate()
+
+注意：仅当以 "r+"   "rb+"    "w"   "wb" "wb+"等以可写模式打开的文件才可以执行该功能。
+"""
 # In[]
 #若文件不存在，系统自动创建。'a'表示可连续写入到文件，保留原内容，在原内容之后写入。可修改该模式（'w+','w','wb'等）
 f = open("./txt_write.txt",'a')
