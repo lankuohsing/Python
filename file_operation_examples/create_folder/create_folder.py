17c70649045613f5750ffdac36e631a12e7a614a
l00467141 <languoxing@huawei.com>
2018-12-01 11:37:03 +0800

    add xreate_fold example

---
+++ file_operation_examples/create_folder/create_folder.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 17:36:20 2018
+
+@author: l00467141
+"""
+# In[]
+import os
+# In[]
+directory="folder1"
+if not os.path.exists(directory):
+    os.makedirs(directory)
