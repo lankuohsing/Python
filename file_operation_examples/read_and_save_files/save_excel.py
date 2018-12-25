6b718eca6ee08eac779a94c65aa3ce221211a9c0
Guoxing Lan <lankuohsing@163.com>
2018-12-25 17:26:00 +0800

    add save_excel

---
+++ file_operation_examples/read_and_save_files/save_excel.py
@@ -0,0 +1,5 @@
+import xlwt
+workbook = xlwt.Workbook(encoding = 'ascii')
+worksheet = workbook.add_sheet('My Worksheet')
+worksheet.write(0, 0, label = 'Row 0, Column 0 Value')
+workbook.save('Excel_Workbook.xls')
