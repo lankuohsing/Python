bcecafd74801bd68d8f98e744c0eab6c59abc830
Guoxing Lan <lankuohsing@163.com>
2019-01-09 18:15:12 +0800

    add openpyxl_save_xlsx.py

---
+++ file_operation_examples/read_and_save_files/openpyxl_save_xlsx.py
@@ -0,0 +1,13 @@
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
+ws1 = wb.create_sheet("Mysheet1") # insert at the end (default)
+# or
+ws2 = wb.create_sheet("Mysheet2", 0) # insert at first position
+ws.title = "New Title"
+ws3 = wb["New Title"]
+ws1["A1"]="A1"
+ws2["A2"]="A2"
+ws['A4'] = 4
+ws['C9'] = 'hello world'
+wb.save('balances.xlsx')
