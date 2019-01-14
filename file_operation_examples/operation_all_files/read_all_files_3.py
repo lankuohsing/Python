55889c26bce5afc34c0a90cf7ccf3eeaa1fda2fd
Guoxing Lan <lankuohsing@163.com>
2019-01-14 15:40:00 +0800

    change author and change os.path.join

--- file_operation_examples/operation_all_files/read_all_files_3.py
+++ file_operation_examples/operation_all_files/read_all_files_3.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Nov 27 20:04:07 2018
 
-@author: l00467141
+@author: lankuohsing
 """
 
 # -*- coding: utf-8 -*-
@@ -13,7 +13,7 @@ def listdir(path, list_name):  #传入存储的list
     for file in os.listdir(path):
         print("file",file)
 #        file_path = os.path.join(path, file)
-        file_path=path+"\\"+file#注意：在windows下，路径分割符为"\"，但在代码中表示它时，要加上转义符号"\"
+        file_path=os.path.join(path,file)#注意：在windows下，路径分割符为"\"，但在代码中表示它时，要加上转义符号"\"
         if os.path.isdir(file_path):
             listdir(file_path, list_name)
         else:
