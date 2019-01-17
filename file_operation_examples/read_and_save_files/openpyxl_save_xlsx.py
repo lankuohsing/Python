6d3744992d2e077820b564c6530d468bbda66695
Guoxing Lan <lankuohsing@163.com>
2019-01-17 17:28:21 +0800

    add append

--- file_operation_examples/read_and_save_files/openpyxl_save_xlsx.py
+++ file_operation_examples/read_and_save_files/openpyxl_save_xlsx.py
@@ -7,6 +7,8 @@ ws2 = wb.create_sheet("Mysheet2", 0) # insert at first position
 ws.title = "New Title"
 ws3 = wb["New Title"]
 ws1["A1"]="A1"
+ws1["B1"]="B1"
+ws1.append(["A2","B2"])
 ws2["A2"]="A2"
 ws['A4'] = 4
 ws['C9'] = 'hello world'
